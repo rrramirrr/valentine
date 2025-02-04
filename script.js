8066b0722a43f09ac394f0d0cf3e5c66313379c6
rrramirrr <a01664841@tec.mx>
2025-02-03 23:12:48 -0600

    3 change

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Segura?",
+    "DE VERDAD??",
+    "POFAVO NO SIGAS...?",
+    "POFAVO...",
+    "DETENTE, ME ESTAS LASTIMANDO",
+    "ME VOY A MORIR",
+    "ESCUCHASTE ESO?",
+    "FUE MI CORAZON ROMPERSEEE",
+    "OKEY YA ENTENDI",
+    "DI QUE SI POFAVO ❤️"
 ];
 
 let messageIndex = 0;
